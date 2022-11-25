ae58a00a780048d9f307f11ec633e931cee8bb61
liang636600 <511922a236i.cdb@sina.cn>
2022-11-25 20:29:01 +0800

    初始化项目

---
+++ src/main/java/com/ucas/ucastack/UcastackApplication.java
@@ -0,0 +1,13 @@
+package com.ucas.ucastack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcastackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcastackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ucas/ucastack/UcastackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucas.ucastack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcastackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
